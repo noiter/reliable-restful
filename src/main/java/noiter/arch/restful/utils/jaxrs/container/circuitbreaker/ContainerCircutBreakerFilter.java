845f071338d6d4eb63c772ecf4044ae638072d4d
Ming Zhao <zm_uestc@hotmail.com>
2015-05-29 18:07:51 +0800

    Init base project

---
+++ src/main/java/noiter/arch/restful/utils/jaxrs/container/circuitbreaker/ContainerCircutBreakerFilter.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2014 Gregor Roth
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package noiter.arch.restful.utils.jaxrs.container.circuitbreaker;
+
+
+
+import java.io.IOException;
+import java.time.Duration;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.ext.Provider;
+
+import noiter.arch.restful.utils.circuitbreaker.CircuitBreakerRegistry;
+import noiter.arch.restful.utils.circuitbreaker.CircuitOpenedException;
+import noiter.arch.restful.utils.circuitbreaker.metrics.MetricsRegistry;
+import noiter.arch.restful.utils.circuitbreaker.metrics.Transaction;
+
+
+
+@Provider
+public class ContainerCircutBreakerFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final String TRANSACTION = "circuit_breaker_transaction";
+
+    private final CircuitBreakerRegistry circuitBreakerRegistry;
+    private final MetricsRegistry metricsRegistry;
+
+
+    @Context 
+    private ResourceInfo resourceInfo; 
+
+    //..
+
+    public ContainerCircutBreakerFilter() {
+        metricsRegistry = new MetricsRegistry();
+        Duration thresholdSlowTransaction = Duration.ofSeconds(5); 
+        circuitBreakerRegistry = new CircuitBreakerRegistry(new OverloadBasedHealthPolicy(metricsRegistry, thresholdSlowTransaction));
+    }
+    
+    
+    
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        String scope = resourceInfo.getResourceClass().getName() + "#" + resourceInfo.getResourceClass().getName(); 
+        
+        if (!circuitBreakerRegistry.get(scope).isRequestAllowed()) {
+            throw new CircuitOpenedException("circuit is open");
+        }
+        
+        Transaction transaction = metricsRegistry.transactions(scope).openTransaction();
+        requestContext.setProperty(TRANSACTION, transaction);
+    }
+
+       
+     
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        boolean isFailed = responseContext.getStatus() >= 500;
+        Transaction.close(requestContext.getProperty(TRANSACTION), isFailed);
+    }
+}
---
+++ src/main/java/noiter/arch/restful/utils/jaxrs/container/circuitbreaker/OverloadBasedHealthPolicy.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2014 Gregor Roth
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package noiter.arch.restful.utils.jaxrs.container.circuitbreaker;
+
+
+import java.time.Duration;
+
+
+import noiter.arch.restful.utils.circuitbreaker.HealthPolicy;
+import noiter.arch.restful.utils.circuitbreaker.metrics.MetricsRegistry;
+import noiter.arch.restful.utils.circuitbreaker.metrics.TransactionMetrics;
+import noiter.arch.restful.utils.jaxrs.container.Environment;
+import noiter.arch.restful.utils.jaxrs.container.Environment.Threadpool;
+import noiter.arch.restful.utils.jaxrs.container.tomcat.TomcatEnvironment;
+
+
+
+public class OverloadBasedHealthPolicy implements HealthPolicy  {
+ 
+    private final Environment environment = new TomcatEnvironment();
+
+    private final MetricsRegistry metricsRegistry;
+    private final Duration thresholdSlowTransaction;
+
+    
+    //...
+    
+    public OverloadBasedHealthPolicy(MetricsRegistry metricsRegistry, Duration thresholdSlowTransaction) {
+        this.metricsRegistry = metricsRegistry;
+        this.thresholdSlowTransaction = thresholdSlowTransaction;
+    }
+
+    
+    
+    @Override
+    public boolean isHealthy(String scope) {
+       
+        // [1] all servlet container threads taken?
+        Threadpool pool = environment.getThreadpoolUsage();
+        if (pool.getCurrentThreadsBusy() >= pool.getMaxThreads()) {
+            TransactionMetrics metrics = metricsRegistry.transactions(scope);
+            
+            // [2] more than 80% currently consumed by this operation?
+            if (metrics.running().size() > (pool.getMaxThreads() * 0.8)) {
+
+                // [3] is 50percentile higher than slow threshold?
+                Duration current50percentile = metrics.ofLast(Duration.ofMinutes(3)).percentile(50);
+                if (thresholdSlowTransaction.minus(current50percentile).isNegative()) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    } 
+}
+
